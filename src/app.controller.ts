0af6302f9f2651055432b8e6071ad82ea0fdd5ef
Jens Spanier <j.spanier@digital-h.de>
2023-11-29 11:15:17 +0100

    Add message to root controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Header } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
+  @Header('Content-Type', 'text/plain')
   getHello(): string {
-    return this.appService.getHello();
+    return ` _____  _____ _____  ______ 
+|  __ \\|_   _|  __ \\|  ____|
+| |__) | | | | |  | | |__   
+|  _  /  | | | |  | |  __|  
+| | \\ \\ _| |_| |__| | |____ 
+|_|  \\_\\_____|_____/|______|
+
+       $NAME-Service
+`;
   }
 }
