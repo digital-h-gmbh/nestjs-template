9002a47c5630ea4d8cc412172b65d3de3eb0635b
Jens Spanier <j.spanier@digital-h.de>
2023-11-29 11:14:57 +0100

    Set port + add v1 prefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = process.env.PORT ?? 9999; // TODO: Change port
+
+  app.setGlobalPrefix('v1', { exclude: ['/'] });
+
+  await app.listen(port);
 }
 bootstrap();
